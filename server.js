2a689b5baac424757fb783e94eba895ed35546a0
Willem Green <willberforce15@gmail.com>
2020-10-20 15:59:47 -0500

    first submission

--- server.js
+++ server.js
@@ -11,8 +11,36 @@ app.use(morgan('dev'))
 app.use(cors())
 app.use(helmet())
 
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+})
+
 app.get('/movie', (req, res) => {
-  res.json(movies)
+  let response = movies;
+
+  if(req.query.genre){
+    response = response.filter(movie => 
+      movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+
+  if(req.query.country){
+    response = response.filter(movie => 
+      movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+
+  if(req.query.avg_vote){
+    response = response.filter(movie =>
+      movie.avg_vote >= req.query.avg_vote)
+  }
+  res.json(response)
 })
 
 const PORT = 8000
