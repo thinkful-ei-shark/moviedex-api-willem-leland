62ab82fe1c2278e6eda33ff022bc988b1c837274
Scott Stone <lelandscottstone33@gmail.com>
2020-10-20 15:12:21 -0500

    Second commit

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log('hello')
+const express = require('express')
+const morgan = require('morgan')
+const movies = require('./movie-data.js')
+const cors = require('cors')
+const helmet = require('helmet')
+require('dotenv').config()
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
+
+app.get('/movie', (req, res) => {
+  res.json(movies)
+})
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
